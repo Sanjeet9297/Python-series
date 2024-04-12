81171fc11c025f77b4f0e3826e52142e0358af14
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2024-04-12 11:14:10 +0530

    first commit

---
+++ day-1/variables/var.py
@@ -0,0 +1,15 @@
+name = "Sanjeet Kumar"  # Strings
+age = 20 
+college = "patel College of Science And Technology"
+cgpa = 7.28
+course = "B.Tech"
+
+print("My name is :", name)
+print("My age is :", age)
+print("My college name is :", college)
+print("My cgpa is :", cgpa)
+print("My course is :", course)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
